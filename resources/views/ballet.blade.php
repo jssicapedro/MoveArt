4e9e745f4156dbdfd9370ba4f177c4497fcdebf0
Jéssica Pedro <jessicapedro165@gmail.com>
2022-11-12 20:56:03 +0000

    alteração de imagens

--- resources/views/ballet.blade.php
+++ resources/views/ballet.blade.php
@@ -4,98 +4,86 @@
 
 @section('hero')
 <section id="hero" class="inner-page d-flex align-items-center">
-    <div class="img_banner">
+    <div class="img_banner" style="background-image: linear-gradient(#eeb2b565,#00000073),url({{ asset('img/dancas/ballet/ballet.png')}}">
         <h1>Ballet</h1>
     </div>
 </section>
 @endsection
 
 @section('main')
-    <div class="about">
-            <div class="txt">
-                <p>O Ballet é uma dança é o estilo de dança mais popular no
-                    mundo que exige muita técnica e precisão nos movimentos, com
-                    a ajuda dos nossos profissionais vais aprender e ser um dos
-                    melhores. </p>
-                <br>
-                <p>A nossa Instituição já foi premiada com ouro em 2020 e 2021.
-                    Queres ser o próximo? Vem connosco e entra nesta aventura.
-                </p>
-            </div>
-            <img src="{{ asset('img/dancas/ballet/ballet_app.png') }}"
-                alt="Criança a treinar ballet de tutu dentro de uma forma irregular sem background e com borda de cor de rosa">
+<div class="about">
+    <div class="txt">
+        <p>O Ballet é uma dança é o estilo de dança mais popular no
+            mundo que exige muita técnica e precisão nos movimentos, com
+            a ajuda dos nossos profissionais vais aprender e ser um dos
+            melhores. </p>
+        <br>
+        <p>A nossa Instituição já foi premiada com ouro em 2020 e 2021.
+            Queres ser o próximo? Vem connosco e entra nesta aventura.
+        </p>
+    </div>
+    <img src="{{ asset('img/dancas/ballet/ballet_app.png') }}" alt="Criança a treinar ballet de tutu dentro de uma forma irregular sem background e com borda de cor de rosa">
+</div>
+<div class="advisors">
+    <h2>Orientadores</h2>
+    <div class="advisors_prof">
+        <div>
+            <img class="grafic1" style="position: absolute;" src="{{ asset('img/dancas/ballet/ballet_prof_.png') }}" alt="">
+            <img class="prof" src="{{ asset('img/dancas/ballet/orientadores(3).png') }}" alt="">
+            <h3>Andreia</h3>
         </div>
-        <div class="advisors">
-            <h2>Orientadores</h2>
-            <div class="advisors_prof">
-                <div>
-                    <img class="grafic1" style="position: absolute;"
-                        src="{{ asset('img/dancas/ballet/ballet_prof_.png') }}" alt="">
-                    <img class="prof" src="{{ asset('img/dancas/ballet/orientadores(3).png') }}" alt="">
-                    <h3>Andreia</h3>
-                </div>
-                <div>
-                    <img class="grafic2" style="position: absolute;"
-                        src="{{ asset('img/dancas/ballet/ballet_prof_.png') }}" alt="">
-                    <img class="prof" src="{{ asset('img/dancas/ballet/orientadores(2).png') }}" alt="">
-                    <h3>Beatriz</h3>
-                </div>
-                <div>
-                    <img class="grafic3" style="position: absolute;"
-                        src="{{ asset('img/dancas/ballet/ballet_prof_.png') }}" alt="">
-                    <img class="prof" src="{{ asset('img/dancas/ballet/orientadores(1).png') }}" alt="">
-                    <h3>André</h3>
-                </div>
-            </div>
+        <div>
+            <img class="grafic2" style="position: absolute;" src="{{ asset('img/dancas/ballet/ballet_prof_.png') }}" alt="">
+            <img class="prof" src="{{ asset('img/dancas/ballet/orientadores(2).png') }}" alt="">
+            <h3>Beatriz</h3>
         </div>
-        <div class="calendar">
-            <h2>Horário</h2>
-            <div id="carousel"
-                class="carousel carousel-dark slide carousel-fade"
-                data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img width="100%" src="{{ asset('img/dancas/calendar.png') }} class="d-block"
-                            alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img width="100%" src="{{ asset('img/dancas/ballet/calendar_ballet.png') }}
-                            class="d-block" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button"
-                    data-bs-target="#carousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon"
-                        aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button"
-                    data-bs-target="#carousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon"
-                        aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+        <div>
+            <img class="grafic3" style="position: absolute;" src="{{ asset('img/dancas/ballet/ballet_prof_.png') }}" alt="">
+            <img class="prof" src="{{ asset('img/dancas/ballet/orientadores(1).png') }}" alt="">
+            <h3>André</h3>
+        </div>
+    </div>
+</div>
+<div class="calendar">
+    <h2>Horário</h2>
+    <div id="carousel" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img width="100%" src="{{ asset('img/dancas/calendar.png') }}" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img width="100%" src="{{ asset('img/dancas/ballet/calendar_ballet.png') }}" class="d-block" alt="...">
             </div>
         </div>
-        <div class="calendar_mobile">
-            <h2>Horário</h2>
-            <div class="image">
-                <img src="{{asset('img/dancas/ballet/calendar_ballet.png') }} data-toggle="modal" data-target="#exampleModal" />
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">
-                                    ×
-                                </span>
-                            </button>
-                            <!--Modal body with image-->
-                            <div class="modal-body">
-                                <img width="100%" src="{{ asset('img/dancas/ballet/calendar_ballet.png') }}" />
-                            </div>
-                        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<div class="calendar_mobile">
+    <h2>Horário</h2>
+    <div class="image">
+        <img src="{{asset('img/dancas/ballet/calendar_ballet.png') }}" data-toggle="modal" data-target="#exampleModal" />
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">
+                            ×
+                        </span>
+                    </button>
+                    <!--Modal body with image-->
+                    <div class="modal-body">
+                        <img width="100%" src="{{ asset('img/dancas/ballet/calendar_ballet.png') }}" />
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
--- resources/views/espanhola.blade.php
+++ resources/views/espanhola.blade.php
@@ -4,98 +4,81 @@
 
 @section('hero')
 <section id="hero" class="inner-page d-flex align-items-center">
-    <div class="img_banner">
+    <div class="img_banner" style="background-image: linear-gradient(#d296d665,#00000073), url({{ asset('img/dancas/espanhola/espanhola.png')}}">
         <h1>Dança Espanhola</h1>
     </div>
 </section>
 @endsection
 
 @section('main')
-    <div class="about">
-            <div class="txt">
-                <p>O Ballet é uma dança é o estilo de dança mais popular no
-                    mundo que exige muita técnica e precisão nos movimentos, com
-                    a ajuda dos nossos profissionais vais aprender e ser um dos
-                    melhores. </p>
-                <br>
-                <p>A nossa Instituição já foi premiada com ouro em 2020 e 2021.
-                    Queres ser o próximo? Vem connosco e entra nesta aventura.
-                </p>
-            </div>
-            <img src="{{ asset('img/dancas/espanhola/espanhola_app.png') }}"
-                alt="Criança a treinar espanhola de tutu dentro de uma forma irregular sem background e com borda de cor de rosa">
+<div class="about">
+    <div class="txt">
+        <p>A dança espanhola é conhecida pelos movimentos elegantes. A dança espanhola mais conhecida é o Flamingo, e é esta à qual a MoveArt dá mais atenção.</p>
+        <br>
+        <p>A nossa Instituição foi premiada em 2018 como a melhor escola de Leiria de Flamingo. Aprende esta modalidade com os nosso profissionais de topo.</p>
+    </div>
+    <img src="{{ asset('img/dancas/espanhola/espanhola_app.png') }}" alt="Criança a treinar espanhola de tutu dentro de uma forma irregular sem background e com borda de cor de rosa">
+</div>
+<div class="advisors">
+    <h2>Orientadores</h2>
+    <div class="advisors_prof">
+        <div>
+            <img class="grafic1" style="position: absolute;" src="{{ asset('img/dancas/espanhola/espanhola_prof_.png') }}" alt="">
+            <img class="prof" src="{{ asset('img/dancas/espanhola/orientadores(3).png') }}" alt="">
+            <h3>Catrina</h3>
         </div>
-        <div class="advisors">
-            <h2>Orientadores</h2>
-            <div class="advisors_prof">
-                <div>
-                    <img class="grafic1" style="position: absolute;"
-                        src="{{ asset('img/dancas/espanhola/espanhola_prof_.png') }}" alt="">
-                    <img class="prof" src="{{ asset('img/dancas/espanhola/orientadores(3).png') }}" alt="">
-                    <h3>Catrina</h3>
-                </div>
-                <div>
-                    <img class="grafic2" style="position: absolute;"
-                        src="{{ asset('img/dancas/espanhola/espanhola_prof_.png') }}" alt="">
-                    <img class="prof" src="{{ asset('img/dancas/espanhola/orientadores(2).png') }}" alt="">
-                    <h3>Íris</h3>
-                </div>
-                <div>
-                    <img class="grafic3" style="position: absolute;"
-                        src="{{ asset('img/dancas/espanhola/espanhola_prof_.png') }}" alt="">
-                    <img class="prof" src="{{ asset('img/dancas/espanhola/orientadores(1).png') }}" alt="">
-                    <h3>Afonso</h3>
-                </div>
-            </div>
+        <div>
+            <img class="grafic2" style="position: absolute;" src="{{ asset('img/dancas/espanhola/espanhola_prof_.png') }}" alt="">
+            <img class="prof" src="{{ asset('img/dancas/espanhola/orientadores(2).png') }}" alt="">
+            <h3>Íris</h3>
         </div>
-        <div class="calendar">
-            <h2>Horário</h2>
-            <div id="carousel"
-                class="carousel carousel-dark slide carousel-fade"
-                data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img width="100%" src="{{ asset('img/dancas/calendar.png') }} class="d-block"
-                            alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img width="100%" src="{{ asset('img/dancas/espanhola/calendar_espanhola.png') }}
-                            class="d-block" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button"
-                    data-bs-target="#carousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon"
-                        aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button"
-                    data-bs-target="#carousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon"
-                        aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+        <div>
+            <img class="grafic3" style="position: absolute;" src="{{ asset('img/dancas/espanhola/espanhola_prof_.png') }}" alt="">
+            <img class="prof" src="{{ asset('img/dancas/espanhola/orientadores(1).png') }}" alt="">
+            <h3>Afonso</h3>
+        </div>
+    </div>
+</div>
+<div class="calendar">
+    <h2>Horário</h2>
+    <div id="carousel" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img width="100%" src="{{ asset('img/dancas/calendar.png') }}" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img width="100%" src="{{ asset('img/dancas/espanhola/calendar_espanhola.png') }}" class="d-block" alt="...">
             </div>
         </div>
-        <div class="calendar_mobile">
-            <h2>Horário</h2>
-            <div class="image">
-                <img src="{{asset('img/dancas/espanhola/calendar_espanhola.png') }} data-toggle="modal" data-target="#exampleModal" />
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">
-                                    ×
-                                </span>
-                            </button>
-                            <!--Modal body with image-->
-                            <div class="modal-body">
-                                <img width="100%" src="{{ asset('img/dancas/espanhola/calendar_espanhola.png') }}" />
-                            </div>
-                        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<div class="calendar_mobile">
+    <h2>Horário</h2>
+    <div class="image">
+        <img src="{{asset('img/dancas/espanhola/calendar_espanhola.png') }}" data-toggle="modal" data-target="#exampleModal" />
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">
+                            ×
+                        </span>
+                    </button>
+                    <!--Modal body with image-->
+                    <div class="modal-body">
+                        <img width="100%" src="{{ asset('img/dancas/espanhola/calendar_espanhola.png') }}" />
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
