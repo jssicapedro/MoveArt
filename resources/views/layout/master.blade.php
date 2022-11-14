17d0b5ddbdca24498c9d0df13f0d7a229865da39
Jéssica Pedro <67608094+jssicapedro@users.noreply.github.com>
2022-11-14 16:00:43 +0000

    funcionamento no pc da escola

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -10,7 +10,7 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" type="image/x-icon">
+  <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
