805e9bab3351d9a3e2d09b73a8323e37389d41a7
Jéssica Pedro <jessicapedro165@gmail.com>
2022-11-17 21:58:06 +0000

    update mapaaulas

--- resources/views/mapaaulas.blade.php
+++ resources/views/mapaaulas.blade.php
@@ -13,68 +13,68 @@
 @section('main')
 
 <section class="gallery">
-                <div class="container">
-                    <h2 class="section__title">
-                        Calendário 
-                    </h2>
-                    <div class="row">
-                        <div class="gallery-filter">
-                            <span class="filter-item active" data-filter="geral">Geral</span>
-                            <span class="filter-item" data-filter="ballet">Ballet</span>
-                            <span class="filter-item" data-filter="hiphop">Hip-hop</span>
-                            <span class="filter-item" data-filter="dancaEsp">Dança Espanhola</span>
-                            <span class="filter-item" data-filter="dancaOrien">Dança Oriental</span>
-                            <span class="filter-item" data-filter="folclore">Folclore</span>
-                        </div>
-                    </div>
-                    <div class="row">
+    <div class="container">
+        <h2 class="section__title">
+            Calendário
+        </h2>
+        <div class="row">
+            <div class="gallery-filter">
+                <span class="filter-item active" data-filter="geral">Geral</span>
+                <span class="filter-item" data-filter="dancaballet">Ballet</span>
+                <span class="filter-item" data-filter="dancaHiphop">Hip-hop</span>
+                <span class="filter-item" data-filter="dancaEsp">Dança Espanhola</span>
+                <span class="filter-item" data-filter="dancaOrien">Dança Oriental</span>
+                <span class="filter-item" data-filter="dancafolclore">Folclore</span>
+            </div>
+        </div>
+        <div class="row">
 
-                        <div class="gallery-item geral">
-                            <div class="gallery-item-inner">
-                                <img src="{{ asset('img/mapaaulas/calendar.png') }}" alt="geral">
-                            </div>
-                        </div>
-                        <div class="gallery-item hiphop">
-                            <div class="gallery-item-inner">
-                                <img src="{{ asset('img/mapaaulas/calendar_hiphop.png') }}" alt="hiphop">
-                            </div>
-                        </div>
-                              <div class="gallery-item dancaOrien">
-                                  <div class="gallery-item-inner">
-                                      <img src="{{ asset('img/mapaaulas/calendar_oriental.png') }}" alt="dancaOrien">
-                                  </div>
-                              </div>
-            
-                        <div class="gallery-item dancaEsp">
-                            <div class="gallery-item-inner">
-                                <img src="{{ asset('img/mapaaulas/calendar_espanhola.png') }}" alt="dancaEsp">
-                            </div>
-                        </div>
-            
-                        <div class="gallery-item ballet">
-                            <div class="gallery-item-inner">
-                                <img src="{{ asset('img/mapaaulas/calendar_ballet.png') }}" alt="ballet">
-                            </div>
-                        </div>
-
-                        <div class="gallery-item folclore">
-                            <div class="gallery-item-inner">
-                                <img src="{{ asset('img/mapaaulas/calendar_folclore.png') }}" alt="folclore">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-         <section >
-            <div id="imagem">
-                <div id="textoimagem">
-                    <h2 class="section__title">
-                        Inscreve-te na nossa associação
-                    </h2>
-                    <button>Novo Registo</button>
+            <div class="gallery-item geral">
+                <div class="gallery-item-inner">
+                    <img src="{{ asset('img/mapaaulas/calendar.png') }}" alt="geral">
                 </div>
             </div>
-         </section>
-         
+            <div class="gallery-item dancaHiphop">
+                <div class="gallery-item-inner">
+                    <img src="{{ asset('img/mapaaulas/calendar_hiphop.png') }}" alt="hiphop">
+                </div>
+            </div>
+            <div class="gallery-item dancaOrien">
+                <div class="gallery-item-inner">
+                    <img src="{{ asset('img/mapaaulas/calendar_oriental.png') }}" alt="dancaOrien">
+                </div>
+            </div>
+
+            <div class="gallery-item dancaEsp">
+                <div class="gallery-item-inner">
+                    <img src="{{ asset('img/mapaaulas/calendar_espanhola.png') }}" alt="dancaEsp">
+                </div>
+            </div>
+
+            <div class="gallery-item dancaballet">
+                <div class="gallery-item-inner">
+                    <img src="{{ asset('img/mapaaulas/calendar_ballet.png') }}" alt="ballet">
+                </div>
+            </div>
+
+            <div class="gallery-item dancafolclore">
+                <div class="gallery-item-inner">
+                    <img src="{{ asset('img/mapaaulas/calendar_folclore.png') }}" alt="folclore">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="mapaaulas_imagem">
+    <div>
+        <div id="textoimagem">
+            <h2 class="section__title">
+                Inscreve-te na nossa associação
+            </h2>
+            <button>Novo Registo</button>
+        </div>
+    </div>
+</section>
+
 @endsection
