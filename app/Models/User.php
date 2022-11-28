947f2947f97bb6af9ce3f693c40f7ed240a2817d
Jéssica Pedro <jessicapedro165@gmail.com>
2022-11-28 23:01:27 +0000

    implemetar BD

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,17 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'primeiro',
+        'apelido',
         'email',
-        'password',
+        'data_nasc',
+        'cc',
+        'genero',
+        'perfil',
+        'modalidade',
+        'type_insc',
+        'metod_pag',
+        'pass',
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('foto');
+            $table->string('primeiro', 30);
+            $table->string('apelido', 30);
+            $table->string('telefone', 9);
+            $table->string('email', 30)->unique;
+            $table->date('data_nasc');
+            $table->string('cc', 9)->unique;
+            $table->string('nif', 9);
+            $table->enum('genero', ['feminino', 'masculino', 'outro', 'nao divulgar']);
+            $table->enum('perfil', ['aluno', 'admin', 'professor'])->default('aluno');
+            $table->string('localidade', 30);
+            $table->string('rua', 30);
+            $table->string('cod_postal', 8);
+            $table->string('modalidade');
+            $table->enum('type_insc', ['anual', 'mensal']);
+            $table->enum('metod_pag', ['paypal', 'visa', 'multibanco', 'MBway']);
+            $table->string('pass');
+
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
