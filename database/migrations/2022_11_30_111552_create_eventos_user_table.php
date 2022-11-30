f2c4748f635b0712e89066a2a3d557c0a2890db0
Jéssica Pedro <67608094+jssicapedro@users.noreply.github.com>
2022-11-30 11:16:41 +0000

    update database

---
+++ database/migrations/2022_11_30_111552_create_eventos_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos_users', function (Blueprint $table) {
+            $table->integer('user_id')->foreign('user_id')->references('id')->on('users');
+            $table->integer('evento_id')->foreign('evento_id')->references('id')->on('eventos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos_users');
+    }
+};
