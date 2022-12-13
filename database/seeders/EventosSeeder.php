24853d7e3bf7cd1b250a1247141cddafb8672d34
beatriz <bea.ag03@gmail.com>
2022-12-13 17:13:30 +0000

    update

--- database/seeders/EventosSeeder.php
+++ database/seeders/EventosSeeder.php
@@ -20,15 +20,22 @@ class EventosSeeder extends Seeder
                 'nome' => 'A bela Adormecida',
                 'data'=> '2022-12-12 20:30:00',
                 'localizacao' => 'Leiria',
-                'foto' => '',
+                'foto' => 'evento1.jpg',
                 'descricao' => 'Espetáculo da história da Bela adormecida com os melhores dançarinos do país.',
             ],
             [
                 'nome' => 'House vs Hip-Hop',
                 'data'=> '2022-12-03 14:45:00',
                 'localizacao' => 'Leiria',
-                'foto' => '',
+                'foto' => 'evento2.jpg',
                 'descricao' => 'Batalha de House vs Hip-Hop. Escolhe a melhor!',
+            ],
+            [
+                'nome' => 'Dança Espanhola',
+                'data'=> '2022-11-29 16:00:00',
+                'localizacao' => 'Leiria',
+                'foto' => 'evento3.jpg',
+                'descricao' => 'Espetáculo de dança espanhola. Não perca!',
             ],
         ]);
     }
