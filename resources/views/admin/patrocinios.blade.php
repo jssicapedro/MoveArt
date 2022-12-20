58f4ec926830b17fe4fa3aa330ea6dd93e4c538a
Jéssica Pedro <jessicapedro165@gmail.com>
2022-12-20 00:32:31 +0000

    update patrocinios

--- resources/views/admin/patrocinios.blade.php
+++ resources/views/admin/patrocinios.blade.php
@@ -6,46 +6,52 @@
 <link rel="stylesheet" href="{{ asset('css/patrocinios_back.css') }}">
 <!-- icon -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-<!-- datatable
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css"> -->
+<!-- datatable -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
 @endsection
 
 @section('main')
 <div class="dashboard_main">
     <div class="patrocinio_main">
         <h1>Patrocínios</h1>
-        <table class="table table-bordered yajra-datatable">
-            <tr>
-                <th>id</th>
-                <th>Patrocinador</th>
-                <th>Mensagem</th>
-                <th>Valor</th>
-                <th>Estado</th>
-                <th>
-                    <span class="material-symbols-outlined">
-                        more_vert
-                    </span>
-                </th>
-            </tr>
+        <table id="patrocinio" class="table table-striped align-middle dataTable no-footer display">
+            <thead>
+                <tr class="title">
+                    <th style="text-align: center;">id</th>
+                    <th style="text-align: center;">Patrocinador</th>
+                    <th style="text-align: center;">Mensagem</th>
+                    <th style="text-align: center;">Valor</th>
+                    <th style="text-align: center;">Estado</th>
+                    <th style="text-align: center;">
+                        <!-- <span class="material-symbols-outlined">
+                            more_vert
+                        </span> -->
+                    </th>
+                </tr>
+            </thead>
             <tbody>
                 @if (count($patrocinios))
                 @foreach($patrocinios as $patrocinio)
                 <tr>
-                    <td>{{$patrocinio->id}}</td>
-                    <td>{{$patrocinio->nome}}</td>
-                    <td class="mensagem">{{$patrocinio->mensagem}}</td>
-                    <td>{{$patrocinio->valor}}</td>
-                    
-                        @if($patrocinio->estado =='sem reposta')
-                            <td><p class="semR">Sem Resposta</p></td>
-                        @elseif($patrocinio->estado =='respondido')
-                            <td><p class="comR">Respondido</p></td>
-                            @else
-                            <h6>Não existem categorias registadas</h6>
-                        @endif
+                    <td style="text-align: center;">{{$patrocinio->id}}</td>
+                    <td style="text-align: center;">{{$patrocinio->nome}}</td>
+                    <td class="mensagem" style="text-align: center;">{{$patrocinio->mensagem}}</td>
+                    <td style="text-align: center;">{{$patrocinio->valor}}</td>
+
+                    @if($patrocinio->estado =='sem reposta')
+                    <td style="text-align: center;">
+                        <p class="semR">Sem Resposta</p>
+                    </td>
+                    @elseif($patrocinio->estado =='respondido')
+                    <td style="text-align: center;">
+                        <p class="comR">Respondido</p>
+                    </td>
+                    @else
+                    <h6>Não existem categorias registadas</h6>
+                    @endif
                     </td>
 
-                    <td>
+                    <td style="text-align: center; width: auto;">
                         <a href="">
                             <span class="material-symbols-outlined">
                                 visibility
@@ -74,6 +80,50 @@
 @endsection
 
 @section('script')
-<!-- <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script> -->
+<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#patrocinio').DataTable({
 
+            searching: false,
+
+            "columns": [
+                null,
+                {
+                    "bSortable": false
+                },
+                {
+                    "bSortable": false
+                },
+                null,
+                null,
+                {
+                    "bSortable": false
+                },
+            ],
+            pageLength: 10,
+            lengthMenu: [
+                [5, 10, 20, -1],
+                [5, 10, 20, 'Todos']
+            ],
+            "order": [
+                [0, 'desc']
+            ],
+            "language": {
+                "lengthMenu": "Entradas: _MENU_ por página",
+                "zeroRecords": "Nothing found - sorry",
+                "info": " _PAGE_ de _PAGES_ ",
+                "infoEmpty": "No records available",
+                "infoFiltered": "(filtered from _MAX_ total records)",
+                "paginate": {
+                    "first": "Primeiro",
+                    "last": "Ultimo",
+                    "next": "Seguinte",
+                    "previous": "Anterior"
+                }
+            }
+
+        });
+    });
+</script>
 @endsection
