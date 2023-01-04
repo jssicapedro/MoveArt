660bb7ba01d5ff880a288b0e15af623fafb29e55
Diogo <ptdiogohd@gmail.com>
2023-01-04 11:17:50 +0000

    contrução do dising do perfil e colocação da imagem

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -6,144 +6,42 @@
     <link href="{{ asset('css/perfil.css') }}" rel="stylesheet">
 @endsection
 
-
-
-
-
-
-
 @section('main')
-
-    <div class="container todas_informacoes_perfil">
-        <div class="titulo_perfil">
-            <h1>Meu Perfil</h1>
-        </div>
-        <div class="infurmcoes_fundamental_perfil">
-            <div class="foto_perfil_perfil">
-                <img src="{{ asset('img/contactos/foncionarios/imagem1.jpg') }}" alt="">
+    <div class="junção_de_tudo_perfil">
+        <div class="bloco_um_da_divisão_do_perfil">
+            <div class="foto_do_perfil_perfi">
+                <img src="{{ asset('storage/profile/' . Auth::user()->foto) }}" alt="">
             </div>
-            <div class="bloco_total_de_texto_perfil">
-                <div class="nome_da_pessoa_perfil">
-                    <p>{{ Auth::user()->primeiro }} {{ Auth::user()->apelido }}</p>
+            <div class="tudo_meu_perfil_perfil">
+                <div class="meu_perfil_perfil">
+                    <h1>Meu Perfil</h1>
                 </div>
-
-                <div class="genero_da_pessoa_perfil">
-                    <p> Genero: {{ Auth::user()->genero }} </p>
-                </div>
-
-                <div class="perfil_da_pessoa_perfil">
-                    <p> perfil: {{ Auth::user()->perfil }} </p>
-                </div>
-
-            </div>
-            <div>
-                    
-            </div>
-        </div>
-        <div class="botoes_iguais_perfil">
-
-            {{-- editar --}}
-
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                Edita
-            </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-                aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Understood</button>
-                        </div>
-                    </div>
+                <div class="texto_do_meu_perfil_perfil">
+                    <p>{{ Auth::user()->localidade }} {{ Auth::user()->rua }} {{ Auth::user()->cod_postal }}, {{ Auth::user()->data_nasc }}</p>
                 </div>
             </div>
-            {{-- fim editar --}}
-
-            {{-- apagar --}}
-
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                apagar
-            </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-                aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Understood</button>
-                        </div>
-                    </div>
+            <div class=" todas_as_informações_da_pessoa_bloco_um">
+                <div class=" nome_da_pessoa_perfil ">
+                    <h2>Nome</h2> 
+                    <p> {{ Auth::user()->primeiro }} {{ Auth::user()->apelido }}</p>                    
                 </div>
+                <hr>
+                <div class=" numero_da_pessoa_perfil ">
+                    <h2>Telefone</h2>
+                    <p> {{ Auth::user()->telefone }} </p>
+                </div>
+                <hr>
+                <div class=" email_da_pessoa_perfil ">
+                    <h2>Email</h2>
+                    <p> {{ Auth::user()->email }} </p>
+                </div>
+                <hr>
             </div>
-
-            {{-- fim apagar --}}
-
-        </div>
-    </div>
-
-    {{-- localidade --}}
-    <div class="segundo_bloco_total_perfil">
-        <div>
-            <div class="titulo_Morada_perfil">
-                <h2>Morada</h2>
-            </div>
-            <div class="localidade_da_pessoa_perfil">
-                <p>Localidade: {{ Auth::user()->localidade }}</p>
-            </div>
-            <hr>
-            <div class=" rua_da_pessoa_perfil">
-                <p>Rua: {{ Auth::user()->rua }}</p>
-            </div>
-            <hr>
-            <div class=" codigo_postal_da_pessoa_perfil">
-                <p>Codigo Postal: {{ Auth::user()->cod_postal }}</p>
+            <div class="frame">
+                <a href=""><button class="custom-btn-perfil btn-1-perfil">Edite</button></a>
             </div>
         </div>
-        <div>
-            <div class="titulo_contactos_perfil">
-                <h2>Contactos</h2>
-            </div>
-            <div class="email_da_pessoa_perfil">
-                <p>Email: {{ Auth::user()->email }}</p>
-            </div>
-            <hr>
-            <div class="telefone_da_pessoa_perfil">
-                <p>Telefone: {{ Auth::user()->telefone }}</p>
-            </div>
-        </div>
-        <div>
-            <div class="Modalidades">
-                <h2>Informações</h2>
-            </div>
-            <div class="cc_da_pessoa_perfil">
-                <p>Cartão de Cidadão: {{ Auth::user()->cc }}</p>
-            </div>
-            <hr>
-            <div class="nif_da_pessoa_perfil">
-                <p>NIF: {{ Auth::user()->nif }}</p>
-            </div>
-        </div>
-
+        <form action=""></form>
     </div>
 @endsection
 
