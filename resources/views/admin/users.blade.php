827ab4cbf10f2f4276ae9241562b2eaaef7c0a39
beatriz <bea.ag03@gmail.com>
2023-01-04 12:45:02 +0000

    update

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -28,11 +28,12 @@
                     <div class="col-6">
                         <div class="buttons d-flex justify-content-end">
                             <div id="add">
-                            <button type="button" class="btn add">+ Adicionar</button>     
+                                <button type="button" class="btn add">+ Adicionar</button>
                             </div>
-                           <div id="load">
-                              <button onClick="window.location.reload();" type="button" class="btn load">Atualizar</button> 
-                           </div>
+                            <div id="load">
+                                <button onClick="window.location.reload();" type="button"
+                                    class="btn load">Atualizar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -77,17 +78,96 @@
                                             <td class="align-middle">{{ $user->genero }}</td>
                                             <td class="align-middle">{{ $user->perfil }}</td>
                                             <td class="align-middle icon_btns"><button data-toggle="collapse"
-                                                    data-target="#demo{{ $user->id }}"
+                                                    data-target="#view{{ $user->id }}"
                                                     class="btn viewbtn accordion-toggle"><i
-                                                        class="fa-solid fa-eye"></i></button><button class="btn editbtn"
-                                                    data-bs-toggle="modal" data-bs-target="#editmodal"><i
-                                                        class="fa-solid fa-pencil"></i></button><button
-                                                    class="btn deletebtn"><i class="fa-solid fa-trash"></i></button></td>
+                                                        class="fa-solid fa-eye"></i></button>
+
+                                                <button class="btn editbtn" data-toggle="collapse"
+                                                    data-target="#edit{{ $user->id }}"><i
+                                                        class="fa-solid fa-pencil"></i></button>
+
+                                                <button class="btn deletebtn"><i class="fa-solid fa-trash"></i></button>
+                                            </td>
                                         </tr>
+                                        <div class="tr_border">
+                                            <tr>
+                                                <td colspan="8" class="hiddenRow">
+                                                    <div id="view{{ $user->id }}" class="accordian-body collapse">
+                                                        <div class="container">
+                                                            <div id="expand_user">
+                                                                <div class="row">
+                                                                    <div class="col-3 nas">
+                                                                        <div id="view_user_photo">
+                                                                            @if ($user->foto == null)
+                                                                                <img src="{{ asset('img/inicio/user.jpg') }}"
+                                                                                    alt="user">
+                                                                            @else
+                                                                                <img src="{{ url('storage/professores/' . $user->foto) }}"
+                                                                                    alt="user">
+                                                                            @endif
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="col-3 nas">
+                                                                        <div class="user_name">
+                                                                            <h2>{{ $user->primeiro }} {{ $user->apelido }}
+                                                                            </h2>
+                                                                        </div>
+                                                                        <div class="user_list">
+                                                                            <ul>
+                                                                                <li><span>Nascimento:
+                                                                                    </span>{{ $user->data_nasc }}</li>
+                                                                                <li><span>Género:
+                                                                                    </span>{{ $user->genero }}
+                                                                                </li>
+                                                                                <li><span>Telefone:
+                                                                                    </span>{{ $user->telefone }}</li>
+                                                                                <li><span>Email: </span>{{ $user->email }}
+                                                                                </li>
+                                                                            </ul>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-3 nas">
+                                                                        <div class="user_list" style="padding-top: 46px">
+                                                                            <ul>
+                                                                                <li><span>CC: </span>{{ $user->cc }}
+                                                                                </li>
+                                                                                <li><span>Nif: </span>{{ $user->nif }}
+                                                                                </li>
+                                                                                <li><span>Localidade:
+                                                                                    </span>{{ $user->localidade }}</li>
+                                                                                <li><span>Rua: </span>{{ $user->rua }}
+                                                                                </li>
+                                                                            </ul>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-3 nas">
+                                                                        <div class="user_list" style="padding-top: 46px">
+                                                                            <ul>
+                                                                                <li><span>Código-Postal:
+                                                                                    </span>{{ $user->cod_postal }}</li>
+                                                                                <li><span>Perfil:
+                                                                                    </span>{{ $user->perfil }}
+                                                                                </li>
+                                                                                <li><span>Modalidade:
+                                                                                    </span>{{ $user->telefone }}</li>
+                                                                            </ul>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+
+                                            </tr>
+                                        </div>
+
+                                        {{-- <div class="tr_border"></div>
                                         <tr>
 
                                             <td colspan="8" class="hiddenRow">
-                                                <div id="demo{{ $user->id }}" class="accordian-body collapse">
+                                                <div id="edit{{ $user->id }}" class="accordian-body collapse">
                                                     <div class="container">
                                                         <div id="expand_user">
                                                             <div class="row">
@@ -105,7 +185,7 @@
 
                                                                 <div class="col-3 nas">
                                                                     <div class="user_name">
-                                                                        <h2>{{ $user->primeiro }} {{ $user->apelido }}
+                                                                        <h2>esta bemmmm
                                                                         </h2>
                                                                     </div>
                                                                     <div class="user_list">
@@ -151,22 +231,23 @@
                                             </td>
 
                                         </tr>
-                                    @endforeach
-                                @else
-                                    <h6>Não existem categorias registadas</h6>
-                                @endif
+                    </div> --}}
+                    @endforeach
+                @else
+                    <h6>Não existem categorias registadas</h6>
+                    @endif
 
-                            </tbody>
+                    </tbody>
 
-                        </table>
-                    </div>
+                    </table>
+                </div>
 
-                    <div class="d-flex justify-content-center">
-                        {!! $users->withQueryString()->links() !!}
-                    </div>
+                <div class="d-flex justify-content-center">
+                    {!! $users->withQueryString()->links() !!}
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 @endsection
