87c51289ef07ae5b2da480c46d0f9a2253af858f
Adriana Valente <83579314+A12252003@users.noreply.github.com>
2023-01-10 16:53:06 +0000

    update show

--- resources/views/admin/modalidades/modalidade_show.blade.php
+++ resources/views/admin/modalidades/modalidade_show.blade.php
@@ -15,4 +15,52 @@
 @endsection
 
 @section('main')
+<div class="dashboard_main">
+    <h1 class="title">
+        <i class="fa-solid fa-eye"></i> Ver Modalidade
+    </h1>
+    <div class="mod_show_main">
+        <form action="{{ route('modalidades.show', $modalidade->id) }}">
+            <div class="row info">
+                <div class="col nome">
+                    <h2>{{$modalidade->modalidade}}</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col descricao">
+                    <p>{{$modalidade->descricao}}</p>
+                </div>
+            </div>
+            <div class="table_valor">
+                <h2>Valores</h2>
+                <div class="valor_mensal">
+                    <h4>Mensal</h4>
+                    <p>{{$modalidade->valor_mensal}}€</p>
+                </div>
+                <div class="valor_anual">
+                    <h4>Anual</h4>
+                    <p>{{$modalidade->valor_anual}}€</p>
+                </div>
+            </div>
+            <div class="row fotos">
+                <div class="col">
+                    <div class="title_fm">Foto Descrição</div>
+                    <img class="img_fm" src="{{ url('storage/modalidades/'.$modalidade->foto_desc) }}" alt="">
+                </div>
+                <div class="col">
+                    <div class="title_fh">Foto do horário</div>
+                    <img class="img_fh" src="{{ url('storage/modalidades/'.$modalidade->foto_horario) }}" alt="">
+                </div>
+                <div class="col">
+                    <div class="title_fmm">Foto modalidade Mensal</div>
+                    <img class="img_fmm" src="{{ url('storage/modalidades/'.$modalidade->foto_mensal) }}" alt="">
+                </div>
+                <div class="col">
+                    <div class="title_fma">Foto modalidade Anual</div>
+                    <img class="img_fma" src="{{ url('storage/modalidades/'.$modalidade->foto_anual) }}" alt="">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
