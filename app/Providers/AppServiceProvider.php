2e1a309c60a354ce6224cfebee620b8e3ba9ea59
Adriana Valente <83579314+A12252003@users.noreply.github.com>
2023-01-23 16:28:31 +0000

    atualização erro 404

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Modalidade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $modalidades_menu = Modalidade::all();
+        view()->share('modalidades_menu', $modalidades_menu);
     }
 }
