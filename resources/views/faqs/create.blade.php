1d557329017b5ea89bc21fa7c0161fa630f9c220
Migueis06 <2211738@my.ipleiria.pt>
2023-01-30 15:15:42 +0000

    d

--- resources/views/faqs/create.blade.php
+++ resources/views/faqs/create.blade.php
@@ -7,41 +7,19 @@
 @endsection
 
 @section('main')
-<div class="dashboard_main">
-        <div class="parteSuperior">
-            <div class="toggle">
-                <ion-icon name="menu-outline"></ion-icon>
-            </div>
-
-            <h1>faqs</h1>
-            <div class="utilizador_dashboard">
-                <span>Olá, admin</span>
-            </div>
-
-        </div>
-
-        <!--dashboard_funcionarios/faqs-->
-        <div class="baixo">
-            <div class="dashboard_funcionarios">
-
-        <h3>Adicionar Evento</h3><br>
-    <form action="{{ url('evento') }}" method="post">
+<div class="card">
+  <div class="card-header">Students Page</div>
+  <div class="card-body">
+      
+      <form action="{{ url('faqs') }}" method="post">
         {!! csrf_field() !!}
-        <label for="">Pergunta</label><br>
-        <input type="text" name="pergunta" placeholder="Pergunta" ><br>
-        <label for="">Resposta</label><br>
-        <input type="text" name="resposta" placeholder="Resposta" ><br>
-        <input type="submit" value="Adicionar"  class="btn btn-success"></br>
+        <label>Name</label></br>
+        <input type="text" name="pergunta" id="pergunta" class="form-control"></br>
+        <label>Name</label></br>
+        <input type="text" name="resposta" id="resposta" class="form-control"></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
-    @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-</div>
+  
+  </div>
 </div>
 @stop
--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -7,73 +7,53 @@
 @endsection
 
 @section('main')
-<div class="dashboard_main">
-        <div class="parteSuperior">
-            <div class="toggle">
-                <ion-icon name="menu-outline"></ion-icon>
+<div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Laravel 9 Crud</h2>
+                    </div>
+                    <div class="card-body">
+                        <a href="{{ url('/faqs/create') }}" class="btn btn-success btn-sm" title="Add New faqs">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                        </a>
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Pergunta</th>
+                                        <th>Resposta</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($faqs as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->pergunta }}</td>
+                                        <td>{{ $item->resposta }}</td>
+                                        <td>
+                                            <a href="{{ url('/faqs/' . $item->id) }}" title="View faqs"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                                            <a href="{{ url('/faqs/' . $item->id . '/edit') }}" title="Edit faqs"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                                            <form method="POST" action="{{ url('/faqs' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                {{ method_field('DELETE') }}
+                                                {{ csrf_field() }}
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete faqs" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <h1>faqs</h1>
-            @auth
-            <div class="utilizador_dashboard">
-                <span>Olá, {{ Auth::user()->primeiro }}</span>
-            </div>@endauth
         </div>
-
-        <!--dashboard_funcionarios/faqs-->
-        <div class="baixo">
-            
-        <h3><a href="{{ url('/evento/create') }}" class="" title="Adicionar Evento"><img src="{{ asset('img/eventosBO/add.png') }}" style="width:30px; height:; " alt=""></a></h3><br>
-        
-        <div class="dashboard_funcionarios">
-        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-        <li class="nav-item">
-            <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true"><img src="{{ asset('img/eventosBO/list.png') }}" style="width:25px; height:; " alt=""></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false"><img src="{{ asset('img/eventosBO/menu.png') }}" style="width:25px; height:; " alt=""></a>
-        </li>
-        </ul>
-        <div class="tab-content" id="pills-tabContent">
-        <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-        <table>
-                    <thead>
-                        <tr>
-                        <th>Foto</th>
-                        <th>Nome</th>
-                        <th>Data</th>
-                        <th>Localização</th>
-                        <th>Descrição</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                    @if (isset($faqs))
-                    @foreach($faqs as $item)
-                        <tr>
-                        <td>
-                            <td>{{ $item->pergunta }}</td>
-                            <td>{{ $item->resposta }}</td>
-                            <td>
-                            <a href="{{ url('/evento/' . $item->id) }}" title="View evento"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i><img src="{{ asset('img/eventosBO/eye.png') }}" style="width:25px; height:; " alt=""></button></a>
-                                <a href="{{ url('/evento/' . $item->id . '/edit') }}" title="Edit evento"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i><img src="{{ asset('img/eventosBO/edit.png') }}" style="width:25px; height:; " alt=""></button></a>
-                                <form method="POST" action="{{ url('/evento' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                    {{ method_field('DELETE') }}
-                                    {{ csrf_field() }}
-                                <button type="submit" class="btn btn-danger btn-sm" title="Delete evento" onclick="return confirm(&quot;De certeza que quer eliminar esta faq?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i><img src="{{ asset('img/eventosBO/bin.png') }}" style="width:25px; height:; " alt=""></button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                    @else
-                <td>Não existem categorias registadas</td>
-                    @endif
-                    </tbody>
-                </table>
-        </div>
-    </div>          
     </div>
-</div>
 @endsection
 
 
