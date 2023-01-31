f30a0f93ce536b2d01f54d7b885d7526c7dc0d96
Jéssica Pedro <jessicapedro165@gmail.com>
2023-01-31 13:34:25 +0000

    update faqs

--- database/seeders/FaqsSeeder.php
+++ database/seeders/FaqsSeeder.php
@@ -17,28 +17,20 @@ class FaqsSeeder extends Seeder
     {
         Faq::insert([
             [
-                'pergunta'=> 'Qual a idade mínima para me inscrever na MoveArta?',
+                'pergunta'=> 'Qual a idade mínima para me inscrever na MoveArt?',
                 'resposta'=> 'Pode se inscrever na nossa associação a partir dos 3 anos.',
-            ],
-            [
-                'pergunta'=> 'Posso me inscrever em mais que uma modalidade?',
-                'resposta'=> 'Sim, pode ser inscrever nas modalidades que pretender.',
-                
             ],
             [
                 'pergunta'=> 'Os produtos da loja são envidados para fora de Portugal?',
                 'resposta'=> 'Não, os produtos são apenas enviados para Portugal continental',
-                
             ],
             [
                 'pergunta'=> 'Onde fica a associação MoveArt?',
                 'resposta'=> 'A associação MoveArt fica em Leiria. A localização exata pode ser vista na página dos contactos.',
-                
             ],
             [
                 'pergunta'=> 'Quantos tipos de dança existem na MoveArt?',
                 'resposta'=> 'Existem 5 tipos de dança',
-                
             ],
         ]);
     }
--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -14,10 +14,10 @@
 @section('main')
 <div class="faqs">
   <h2>FAQs</h2>
+  @if (isset($faqs))
+  @foreach($faqs as $faq)
   <div class="conteudo">
     <div class="pergunta-resposta">
-    @if (isset($faqs))
-  @foreach($faqs as $faq)
       <button id="botao-pergunta1" aria-expanded="false">
         <span class="pergunta">{{$faq->pergunta}}</span>
         <span class="icon" aria-hidden="true"></span>
@@ -26,13 +26,15 @@
         <p>{{$faq->resposta}}</p>
       </div>
     </div>
-    @endforeach
-    @else
-    <h6>Não existem dados registados</h6>
-    @endif
   </div>
+</div>
+@endforeach
+@else
+<h6>Não existem dados registados</h6>
+@endif
+
 @endsection
 
-  @section('script')
-  <script src="{{ asset('js/faqs/faqs.js') }}"></script>
-  @endsection
+@section('script')
+<script src="{{ asset('js/faqs/faqs.js') }}"></script>
+@endsection
