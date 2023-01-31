a3d57d1c2a4ab29b99f06bcb633f450468b0de2e
Migueis06 <2211738@my.ipleiria.pt>
2023-01-31 12:25:43 +0000

    sa

--- database/seeders/FaqsSeeder.php
+++ database/seeders/FaqsSeeder.php
@@ -17,12 +17,27 @@ class FaqsSeeder extends Seeder
     {
         Faq::insert([
             [
-                'pergunta'=> 'skjaxkjshaxkjh',
-                'resposta'=> 'jhxgcjhgzxjhcgx',
+                'pergunta'=> 'Qual a idade mínima para me inscrever na MoveArta?',
+                'resposta'=> 'Pode se inscrever na nossa associação a partir dos 3 anos.',
             ],
             [
-                'pergunta'=> 'zdczdczczdc',
-                'resposta'=> 'czcdzcdzcdzczdczdc',
+                'pergunta'=> 'Posso me inscrever em mais que uma modalidade?',
+                'resposta'=> 'Sim, pode ser inscrever nas modalidades que pretender.',
+                
+            ],
+            [
+                'pergunta'=> 'Os produtos da loja são envidados para fora de Portugal?',
+                'resposta'=> 'Não, os produtos são apenas enviados para Portugal continental',
+                
+            ],
+            [
+                'pergunta'=> 'Onde fica a associação MoveArt?',
+                'resposta'=> 'A associação MoveArt fica em Leiria. A localização exata pode ser vista na página dos contactos.',
+                
+            ],
+            [
+                'pergunta'=> 'Quantos tipos de dança existem na MoveArt?',
+                'resposta'=> 'Existem 5 tipos de dança',
                 
             ],
         ]);
